9626839c4beab9bb140daaaf6557cf124be0d565
20136819 <20136819@tafe.wa.edu.au>
2025-08-05 16:26:30 +0800

    feat(step3): add PlayerList

---
+++ app/player_list.py
@@ -0,0 +1,15 @@
+# -------------------------------
+#
+# Folder: 
+# Filename: 
+# Author: Ayano SASAKIDO
+# Version 0.0
+# Created: 
+#
+# -------------------------------
+
+from player import Player
+
+class PlayerList:
+    def __init__(self, _head=None):
+        self.head = _head
