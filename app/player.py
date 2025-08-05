f9ee624ec412dbdf3bac5a80362f794625ed09d8
20136819 <20136819@tafe.wa.edu.au>
2025-08-05 16:17:45 +0800

    feat(step2): create player class and add 2 tests

---
+++ app/player.py
@@ -0,0 +1,23 @@
+# -------------------------------
+#
+# Folder: app
+# Filename: player.py
+# Author: Ayano SASAKIDO
+# Version 0.0
+# Created: 05/08/2025
+#
+# -------------------------------
+
+class Player():
+    def __init__(self, _uid:str, _name:str):
+        self.uid =_uid
+        self.name =_name
+
+    def uid(self):
+        return self.uid
+
+    def name(self):
+        return self.name
+
+    def __str__(self):
+        return f"ID[ {self.uid}]: {self.name}"
---
+++ test/player_test.py
@@ -0,0 +1,22 @@
+# -------------------------------
+#
+# Folder: test
+# Filename: player_test.py
+# Author: Ayano SASAKIDO
+# Version 0.0
+# Created: 
+#
+# -------------------------------
+
+from app.player import Player
+from unittest import TestCase
+
+class TestPlayer(TestCase):
+
+    def test_player_uid(self):
+        player = Player("08", "Ayano")
+        self.assertEqual(player.uid, "08")
+
+    def test_player_name(self):
+        player = Player("13", "John")
+        self.assertEqual(player.name, "John")
