6aafb8abba0e0fce88484e69e24e492be77e9bb7
20136819 <20136819@tafe.wa.edu.au>
2025-08-22 22:37:16 +0800

    update player.py

--- app/player.py
+++ app/player.py
@@ -9,26 +9,36 @@
 # -------------------------------
 
 class Player:
-    def __init__(self, _uid:str, _name:str):
-        self.uid =_uid
-        self.name =_name
-        self.info = (_uid, _name)
+    def __init__(self, uid:str, name:str):
+        self._uid = uid
+        self._name = name
+        self.info = (uid, name)
 
     @property
     def uid(self):
+        print("getter for uid called")
         return self._uid
 
     @uid.setter
     def uid(self, value:str):
+        print("setter for uid called")
         self._uid = value
 
     @property
     def name(self):
+        print("getter for name called")
         return self._name
 
     @name.setter
     def name(self, value:str):
+        print("setter for name called")
         self._name = value
 
     def __str__(self):
         return f"ID[{self.uid}]: {self.name}"
+
+
+player = Player("101", "Test")
+print(player)
+player.uid = "201"
+print(player.uid)
--- main.py
+++ main.py
@@ -3,24 +3,29 @@ from app.player_node import PlayerNode
 from app.player_list import PlayerList
 
 
-player = Player("2","Test")
-print(f"Player info: {player}")
-node = PlayerNode(player)
-print(f"Node info: {node}")
+# player = Player("2","Test")
+# print(f"Player info: {player}")
+# node = PlayerNode(player)
+# print(f"Node info: {node}")
 
-print(node.key())
+# print(node.key())
 
-list = PlayerList()
-list.insert_tail(1, "test1")
-list.insert_tail(2, "test2")
-list.insert_tail(3, "test3")
+# list = PlayerList()
+# list.insert_tail(1, "test1")
+# list.insert_tail(2, "test2")
+# list.insert_tail(3, "test3")
 
 
 
-list.find_tail()
+# list.find_tail()
 
-print("List forward:")
-list.display(True)
+# print("List forward:")
+# list.display(True)
 
-print("List backward:")
-list.display(False)
+# print("List backward:")
+# list.display(False)
+
+player = Player("101", "Test")
+print(player) # call __str__
+player.uid = "201" # setter uid
+print(player.uid) # getter uid
