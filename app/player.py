05d79cb414f0f0b8b530394f2ee085ddf989beba
20136819 <20136819@tafe.wa.edu.au>
2025-10-03 10:38:59 +0800

    fix: fixed player.py

--- app/player.py
+++ app/player.py
@@ -9,29 +9,25 @@
 # -------------------------------
 
 class Player:
-    def __init__(self, uid:str, name:str, hash=None):
+    def __init__(self, uid:str, name:str):
         self._uid =uid
         self._name =name
         self.info = (uid, name)
 
     @property
     def uid(self):
-        #print("getter for uid called")
         return self._uid
 
     @uid.setter
     def uid(self, value: str):
-        #print("setter for uid called")
         self._uid = value
 
     @property
     def name(self):
-        #print("getter for name called")
         return self._name
 
     @name.setter
     def name(self, value: str):
-        #print("setter for name called")
         self._name = value
 
     def __str__(self):
@@ -40,12 +36,19 @@ class Player:
     def __hash__(self):
         return self.my_hash(self.uid)
 
-    @classmethod
-    def my_hash(cls, key: str) -> int:
+    @staticmethod
+    def my_hash(key: str) -> int:
         try:
             return int(key)
         except ValueError:
             return hash(key)
 
     def __eq__(self, other):
+        """
+        In this case, isinstance is used,
+        so it returns False when comparing objects do not have uid.
+        If isinstance is omitted, it returns an AttributeError.
+        """
         return isinstance(other, Player) and self.uid == other.uid
+
+
